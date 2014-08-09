05ed5336d26c706d6794ea6b4aa7422526ab5650
Jerzy Baran <georgezbaran@gmail.com>
2014-08-09 11:53:35 +0100

    points now get calculated based on player's hand

--- src/player.cpp
+++ src/player.cpp
@@ -30,7 +30,59 @@ void Player::emptyHand() {
 
 int Player::getPoints() {
 
-	return 0;
+    int points = 0; //initialise to 0, otherwise it creates undefined behaviour
+
+	//since aces have a special meaning to points, move them to the end of the
+    //hand and deal with them last
+    for(int i = 0; i < (int)(playerHand.size() / 2); i++){
+
+        if(playerHand.at(i).getCardId() == 0){ //ace
+
+            for(int k = playerHand.size(); i > (int)(playerHand.size() / 2); i--){
+
+                if(playerHand.at(k).getCardId() != 0){
+
+                    //swap
+                    Card temp = playerHand.at(i);
+                    playerHand.at(i) = playerHand.at(k);
+                    playerHand.at(k) = temp;
+
+                    //look for the next ace
+                    break;
+                }
+            }
+        }
+    }
+
+    //iterate through all cards in hand and add the points
+    //ace counts as eleven unless it goes over 21, then it counts as one
+    //2-9 count as their face values
+    //10, jack, queen and king count as 10
+
+    for(int i = 0; i < playerHand.size(); i++) {
+
+        //2-9
+        if(playerHand.at(i).getCardId() > 0 && playerHand.at(i).getCardId() < 9){
+
+            points += (playerHand.at(i).getCardId() + 1);
+        }
+        //10, jack, queen, king
+        else if(playerHand.at(i).getCardId() > 8 &&
+                playerHand.at(i).getCardId() < 13){
+
+            points += 10;
+        }
+        //ace
+        else if(playerHand.at(i).getCardId() == 0){
+
+            if(points + 11 <= 21)
+                points += 11;
+            else
+                points++;
+        }
+    }
+
+    return points;
 }
 
 void Player::printHand() {
@@ -39,6 +91,7 @@ void Player::printHand() {
 	for(int i = 0; i < playerHand.size(); i++) {
 
         printf("%s of %s\n",
-            playerHand.at(i).getVerboseValue(), playerHand.at(i).getSuit());
+            playerHand.at(i).getVerboseValue(),
+            playerHand.at(i).getSuit());
     }
 }
